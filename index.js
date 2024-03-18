e5718b5c891686e7c7835e534506090de218507b
Prithviraj Kalaiselvan <prithviraj792000@gmail.com>
2024-03-18 11:47:29 -0400

    Updated PORTS.

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ require("./startup/routes")(app);
 // Database Connection
 require("./startup/db")();
 
-app.listen(3000, console.log("Listening on Port 3000"));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log(`Listening on Port ${PORT}`));
